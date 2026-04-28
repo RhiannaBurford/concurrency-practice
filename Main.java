d5ad4083521fe53a97cdfa1677d5d27d9ac1a900
Rhianna Burford <rhiannaburford@hotmail.co.uk>
2026-04-28 16:36:12 +0100

    Remember to implement Runnable and @Override

--- Main.java
+++ Main.java
@@ -2,23 +2,9 @@ public class Main {
 
     public static void main (String[] args){
 
-        Worker2 w = new Worker2();
-        Thread t1 = new Thread(w);
+        TimerTask t = new TimerTask();
+        Thread t1 = new Thread(t);
         t1.start();
-        try{
-            Thread.sleep(2000);
-        }
-        catch (InterruptedException e){
-            System.out.println("Error in thread.");
-        }
-        t1.interrupt();
-        try {
-            t1.join();
-        }
-        catch (InterruptedException e){
-            System.out.println("Error in thread.");
-        }
-        
-        System.out.println("Main program finished");
+        System.out.println("Countdown started!");
     }
 }
---
+++ TimerTask.java
@@ -0,0 +1,18 @@
+public class TimerTask implements Runnable {
+
+
+    @Override 
+    public void run(){
+
+        for (int i = 3; i > 0; i--){
+            System.out.println(i + "...");
+            try{
+                Thread.sleep(1000);
+            }
+            catch (InterruptedException e){
+                System.out.println("Error in thread.");
+            }
+        }
+    }
+
+}
