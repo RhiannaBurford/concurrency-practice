a7826e2f592102dd0a58af8c4fb13d80d72afd4e
Rhianna Burford <rhiannaburford@hotmail.co.uk>
2026-04-28 15:10:37 +0100

    For the runnable interface class, use @Override and run()

---
+++ Editor.java
@@ -0,0 +1,15 @@
+public class Editor implements Runnable{
+
+    private Document d;
+
+    public Editor(Document d){
+        this.d = d;
+    }
+
+    @Override 
+    public void run(){
+        for (int i = 0; i < 1000; i++){
+            d.addWords(i);
+        }
+    }
+}
